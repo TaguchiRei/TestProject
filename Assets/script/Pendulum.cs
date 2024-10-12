ee55e85125434a801f7c4f5a80057ffcae113a1a
TaguchiRei <ren.len2426@gmail.com>
2024-10-12 18:58:21 +0900

    振り子についていくつか検証。

--- Assets/script/Pendulum.cs
+++ Assets/script/Pendulum.cs
@@ -11,14 +11,13 @@ public class Pendulum : MonoBehaviour
         transform.LookAt(_fulcrum);
         float gravityPower = 9.81f * _runoutStrength * -1;
         float dis = Vector3.Distance(_fulcrum.position, transform.position);
-        float addPower = Mathf.Floor((dis - distanceForFulcrum));
+        float addPower = Mathf.Floor((dis - distanceForFulcrum)) *1.5f;
         if (transform.position.y > _fulcrum.position.y)
         {
             addPower *= -1;
         }
-        Vector3 gravity = new(0, gravityPower, 0);
-        Vector3 rope = transform.TransformDirection(0, 0, gravityPower * -1);
-        Vector3 setPos = new(0,addPower,0);
-        _rig.AddForce(gravity + rope + setPos, ForceMode.Acceleration);
+        Vector3 rope = transform.TransformDirection(0, 0, 9.81f);
+        Vector3 setPos = new(0, addPower, 0);
+        _rig.AddForce(rope + setPos, ForceMode.Acceleration);
     }
 }
